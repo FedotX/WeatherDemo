875c579404babe2b6e45958e80bdd6386cb61386
FedotX <fedotovsvyatoslav@mail.ru>
2023-05-26 02:24:39 +0500

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Святослав Федотов on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
